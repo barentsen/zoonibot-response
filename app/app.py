c84619adc16884d974af75aef7a20f309ce863be
Geert Barentsen <geert@barentsen.be>
2013-09-17 23:55:45 +0100

    initial commit

--- app/app.py
+++ app/app.py
@@ -12,14 +12,25 @@ zoonibotapp = Flask('zoonibot', static_url_path='')
 
 @zoonibotapp.route('/')
 def root():
-    return "Hello bro. I am Zoonibot. Keep calm and classify on."
+    return """ZooniBot says: "Hello. I am ZooniBot."""
 
-@zoonibotapp.route('/area', methods=['GET'])
-def area():
+@zoonibotapp.route('/examples')
+def examples():
+    return zoonibotapp.send_static_file('examples.html')
+
+@zoonibotapp.route('/gz_summary', methods=['GET'])
+def gz():
     ra = request.args.get('RA', default='', type=float)
     dec = request.args.get('DEC', default='', type=float)
-    sr = request.args.get('SR', default='0.01', type=float)
-    return zoonibot.area_response(ra, dec, sr)
+    sr = request.args.get('SR', default=0.01, type=float)
+    return 'ZooniBot says: "'+zoonibot.galaxyzoo_response(ra, dec, sr)+'"'
+
+@zoonibotapp.route('/ph_candidate', methods=['GET'])
+def ph():
+    ra = request.args.get('RA', default='', type=float)
+    dec = request.args.get('DEC', default='', type=float)
+    #sr = request.args.get('SR', default=0.01, type=float)
+    return 'ZooniBot says: "'+zoonibot.planethunters_response(ra, dec)+'"'
 
 if __name__ == "__main__":
     zoonibotapp.debug = True
--- app/zoonibot.py
+++ app/zoonibot.py
@@ -12,12 +12,6 @@ sdss_typedefs = {0: 'unknowns',
                  4: 'ghosts',
                  6: 'stars'}
 
-def count_unique(keys):
-    """Identifies unique values and counts their occurences."""
-    uniq_keys = np.unique(keys)
-    bins = uniq_keys.searchsorted(keys)
-    return uniq_keys, np.bincount(bins)
-
 def myconesearch(ra, dec, sr, catname="SDSS DR8 - Sloan Digital Sky Survey Data Release 8 2"):
     try:
         response = conesearch.conesearch(ra, dec, sr, catalog_db=catname)
@@ -25,23 +19,48 @@ def myconesearch(ra, dec, sr, catname="SDSS DR8 - Sloan Digital Sky Survey Data 
         return None
     return response.array    
 
-def area_response(ra, dec, sr=0.01):
+def count_unique(keys):
+    """Identifies unique values and counts their occurences."""
+    uniq_keys = np.unique(keys)
+    bins = uniq_keys.searchsorted(keys)
+    return uniq_keys, np.bincount(bins)
+
+def galaxyzoo_response(ra, dec, sr=0.01):
+    """ZooniBot's response to a GZ user who spots 'weird things'.
+    """
     data = myconesearch(ra, dec, sr)
+
     if data == None:
         return "There's nothing in this part of the sky."
-
-    mask = (data['mode'] == 1)
-
-    response = "I am Zoonibot. This area contains "
-
+    # Which object from SDSS' PhotoObjAll do we want to include?
+    mask = ((data['mode'] == 1)
+             & (data['i'] < 20))
+    # Count number of object per type
     mytypes, mycounts = count_unique(data[mask]['type'].data)
     contents = []
     for i in np.argsort(mycounts)[::-1]:
         contents.append("{0} {1}".format(mycounts[i],
                                          sdss_typedefs[mytypes[i]]))
-
+    # Parse Zoonibot's response
+    response = "This region contains about "
     result = response+" and ".join(contents)+"."
+
+    if np.any(data[mask]['i'] < 14):
+        result += ' One of the stars is very bright and might be causing artefacts.'
     return result
 
+def planethunters_response(ra, dec):
+    """ZooniBot's response to a transit candidate.
+    """
+    result = """Thanks for identifying a possible transit!
+                Be aware however, that transits are sometimes caused by the contamination of nearby stars.
+                For this reason, please check 
+                http://surveys.roe.ac.uk:8080/wsa/GetImage?ra={0}&dec={0}&database=wserv4v20101019&frameType=stack&obsType=object&programmeID=10209&mode=show&archive=%20wsa&project=wserv4""".format(ra, dec)
+    
+#result = "http://skyview.gsfc.nasa.gov/cgi-bin/images?Position=60,50&Survey=DSS2R&Return=JPG&Invert=True"
+
+    return result
+    
 if __name__ == '__main__':
-    print zoonibot_area_response(4*15, +50, 0.01)
+    # Example: http://talk.galaxyzoo.org/#/subjects/AGZ00045p8
+    print galaxyzoo_response(102.840897955775, 39.1928898447807, 0.01)
